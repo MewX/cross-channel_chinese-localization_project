3552a38d713ad6efcf6b18b4736086ee7aa91e9d
MewX <imewx@qq.com>
2015-02-26 01:21:48 +0800

    make homepage

---
+++ js/top_motion_edgeActions.js
@@ -0,0 +1,14 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'flare_repeat'
+(function(symbolName){Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","complete",function(sym,e){sym.play();});
+//Edge binding end
+})("flare_repeat");
+//Edge symbol end:'flare_repeat'
+})(jQuery,AdobeEdge,"top-motion");
