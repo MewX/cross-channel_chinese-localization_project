a36a8b2e1e6aa77cfd61dbd0153eb046804ff0b1
MewX <imewx@qq.com>
2015-09-12 16:28:53 +0800

    rework with bootstrap

---
+++ js/main.js
@@ -0,0 +1,55 @@
+jQuery(function($) {
+
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 10000,
+			pause: false
+		});
+	});
+
+	//Ajax contact
+	var form = $('.contact-form');
+	form.submit(function () {
+		$this = $(this);
+		$.post($(this).attr('action'), function(data) {
+			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+		},'json');
+		return false;
+	});
+
+	//smooth scroll
+	$('.navbar-nav > li').click(function(event) {
+		event.preventDefault();
+		var target = $(this).find('>a').prop('hash');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 500);
+	});
+
+	//scrollspy
+	$('[data-spy="scroll"]').each(function () {
+		var $spy = $(this).scrollspy('refresh')
+	})
+
+	//PrettyPhoto
+	$("a.preview").prettyPhoto({
+		social_tools: false
+	});
+
+	//Isotope
+	$(window).load(function(){
+		$portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector : 'li',
+			layoutMode : 'fitRows'
+		});
+		$portfolio_selectors = $('.portfolio-filter >li>a');
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
+});
